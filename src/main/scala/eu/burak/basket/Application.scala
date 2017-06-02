23539827b80f7b4af2aff78affbefdfd9c97049e
karaoglan <e0825659@student.tuwien.ac.at>
2017-06-02 19:24:03 +0300

    scala initial commit

---
+++ src/main/scala/eu/burak/basket/Application.scala
@@ -0,0 +1,23 @@
+package eu.burak.basket
+
+/**
+  * Created by bk on 02/06/2017.
+  */
+object Application {
+
+  def main(args: Array[String]) = {
+    var variable = 2
+    variable += 3
+    println(variable)
+    val kostante = 2
+
+    val text: String = "Text"
+    var text2: String = "Text"
+
+    var preis = 2
+    lazy val lazyBespiel = preis
+    preis += 3
+    println(lazyBespiel)
+  }
+
+}
